0cb72e8cdd5aaa1f308d5637a70d5b96a134fb9b
Avhirup Chakraborty <avhirup84jazz@gmail.com>
2017-10-31 10:39:13 +0530

    Network Imported

---
+++ Network.py
@@ -0,0 +1,9 @@
+import torch
+
+class PGGAN(object):
+	"""docstring for PGGAN"""
+	def __init__(self, least_dimension,max_dimension,dimension_step_ratio):
+		super(PGGAN, self).__init__()
+		self.arg = arg
+
+
